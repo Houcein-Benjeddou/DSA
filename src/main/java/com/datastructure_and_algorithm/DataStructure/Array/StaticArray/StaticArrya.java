66e2cc52a25e187251c9927fc3cd51aeae58ec0e
houcein benjeddou <hou52cine@gmail.com>
2023-04-03 11:49:09 +0100

    init

--- src/main/java/com/datastructure_and_algorithm/DataStructure/Array/StaticArray/StaticArrya.java
+++ src/main/java/com/datastructure_and_algorithm/DataStructure/Array/StaticArray/StaticArrya.java
@@ -2,6 +2,6 @@ package com.datastructure_and_algorithm.DataStructure.Array.StaticArray;
 
 public class StaticArrya {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("test2"); 
     }
 }
