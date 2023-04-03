f55e24cb1bcfdfe90cb4a5e73f3c9aa4c9679589
houcein benjeddou <houceinbenjeddou@gmail.com>
2023-04-03 11:39:09 +0100

    init commit

---
+++ src/main/java/com/datastructure_and_algorithm/DataStructure/Array/StaticArray/StaticArrya.java
@@ -0,0 +1,7 @@
+package com.datastructure_and_algorithm.DataStructure.Array.StaticArray;
+
+public class StaticArrya {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
